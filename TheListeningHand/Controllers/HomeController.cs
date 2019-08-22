595b857339fb677c3032cac85a00b3515a56d1cc
Murray Paton <muzpat@hotmail.com>
2019-08-22 20:05:26 +0100

    Mid Dev checkpoint

--- TheListeningHand/Controllers/HomeController.cs
+++ TheListeningHand/Controllers/HomeController.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheListeningHand.Models;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace TheListeningHand.Controllers
 {
     public class HomeController : Controller
     {
+
+        public class bookingview
+        {
+            public string stylist { get; set; }
+            public string customer { get; set; }
+            public string date { get; set; }
+            public string datedetail { get; set; }
+            public string time { get; set; }
+            public string phone { get; set; }
+            public string style { get; set; }
+            public string info { get; set; }
+
+        }
         public ActionResult Index()
         {
+            TheListeningHand.Models.TheListeningHandEntities context = new TheListeningHandEntities();
+            List<stylist> stylists = context.stylists.ToList();
+            string myText = "<select id=\"postage\"><option value=\"0\">Please select...</option><option value=\"1\">Anyone Available</option> ";
+            foreach (stylist dresser in stylists)
+            {
+                myText = myText + "<option value=\"" + dresser.id + "\">" + dresser.name + "</option>";
+            }
+            myText = myText + " </select>";
+            ViewBag.dropdowntext = myText;
             return View();
         }
 
         public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult Users()
         {
             ViewBag.Message = "Your application description page.";
 
@@ -38,6 +73,413 @@ namespace TheListeningHand.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Appointment(string stylist)
+        {
+
+            List<bookingview> daycal = new List<bookingview>();
+            if (stylist == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.Stylist = stylist;
+
+            Bookings myBookings = new Bookings();
+            List<Booking> thebook = myBookings.GetBookings();
+
+            List<string> myDates = new List<string>();
+            List<string> myDays = new List<string>();
+            //    myDates.Add(DateTime.Today.ToShortDateString());
+            int i = 0;
+            while (i <= 6)
+            {
+                int j = 9;
+                TheListeningHand.Models.TheListeningHandEntities context = new TheListeningHandEntities();
+                List<availability> slots = context.availabilities.Where(s => s.stylistId.ToString() == stylist && s.dayId == 1).ToList();
+                foreach (availability slot in slots)
+                {
+                    bookingview calEntry = new bookingview();
+                    calEntry.date = DateTime.Today.AddDays(i).ToShortDateString();
+                    if (i == 0)
+                    {
+                        calEntry.datedetail = "Today";
+                    }
+                    else
+                    {
+                        calEntry.datedetail = DateTime.Today.AddDays(i).DayOfWeek.ToString();
+                    }
+                    var timeStr = slot.time.ToString() + ":00";
+                    if (j < 10)
+                    {
+                        timeStr = "0" + timeStr;
+                    }
+                    calEntry.time = timeStr;
+                    calEntry.stylist = stylist;
+                    calEntry.customer = "";
+                    var result = thebook.Find(x => x.date == DateTime.Today.AddDays(i).ToShortDateString() && x.time == timeStr && x.stylist == stylist);
+
+                    if (result != null)
+                    {
+                        calEntry.customer = result.name;
+                        calEntry.phone = result.phone;
+                        calEntry.style = result.style;
+                        calEntry.info = result.info;
+                    }
+                    daycal.Add(calEntry);
+                    j++;
+                }
+
+       //         while (j <= 17)
+       //         {
+       //             bookingview calEntry = new bookingview();
+       //             calEntry.date = DateTime.Today.AddDays(i).ToShortDateString();
+       //             if (i == 0)
+       //             {
+       //                 calEntry.datedetail = "Today";
+       //             }
+       //             else
+       //             {
+       //                 calEntry.datedetail = DateTime.Today.AddDays(i).DayOfWeek.ToString();
+       //             }
+       //             var timeStr = j.ToString() + ":00";
+       //             if (j == 9)
+       //             {
+       //                 timeStr = "0" + timeStr;
+       //             }
+       //             calEntry.time = timeStr;
+       //             calEntry.stylist = stylist;
+       //             calEntry.customer = "";
+       //             var result = thebook.Find(x => x.date == DateTime.Today.AddDays(i).ToShortDateString() && x.time == timeStr && x.stylist == stylist);
+       //
+       //             if (result != null)
+       //             {
+       //                 calEntry.customer = result.name;
+       //                 calEntry.phone = result.phone;
+       //                 calEntry.style = result.style;
+       //                 calEntry.info = result.info;
+       //             }
+       //             daycal.Add(calEntry);
+       //             j++;
+       //         }
+
+                myDates.Add(DateTime.Today.AddDays(i).ToShortDateString());
+                i++;
+
+            }
+            ViewBag.BookingDates = myDates;
+
+            myDays.Add("Today");
+            i = 1;
+            while (i <= 6)
+            {
+                myDays.Add(DateTime.Today.AddDays(i).DayOfWeek.ToString());
+                i++;
+
+            }
+            ViewBag.BookingDays = myDays;
+            return View("Appointment");
+
+
+        }
+        [HttpGet]
+        public ActionResult GetUsers(string stylist)
+        {
+            TheListeningHand.Models.TheListeningHandEntities context = new TheListeningHandEntities();
+            List<stylist> stylists = context.stylists.ToList();
+            return Json(stylists, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult UpdateUser(int stylist, string arrayhrs )
+        {
+            dynamic table = JsonConvert.DeserializeObject(arrayhrs);
+            string hrs = "";
+            int i = 0;
+            foreach (var item in  table.hrs)
+            {
+                i++;
+                if (item == true)
+                {
+                    hrs = hrs + i.ToString() + ",";
+                }   
+            }
+            TheListeningHand.Models.TheListeningHandEntities context = new TheListeningHandEntities();
+            List<stylist> stylists = context.stylists.ToList();
+            return Json(stylists, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult GetAppointmentsAnyone(string stylist)
+        {
+
+            List<bookingview> daycal = new List<bookingview>();
+            if (stylist == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.Stylist = stylist;
+
+            Bookings myBookings = new Bookings();
+            List<Booking> thebook = myBookings.GetBookings();
+
+            List<string> myDates = new List<string>();
+            List<string> myDays = new List<string>();
+            //    myDates.Add(DateTime.Today.ToShortDateString());
+            int i = 0;
+            while (i <= 6)
+            {
+                int j = 9;
+                while (j <= 17)
+                {
+                    bookingview calEntry = new bookingview();
+                    calEntry.date = DateTime.Today.AddDays(i).ToShortDateString();
+                    if (i == 0)
+                    {
+                        calEntry.datedetail = "Today";
+                    }
+                    else
+                    {
+                        calEntry.datedetail = DateTime.Today.AddDays(i).DayOfWeek.ToString();
+                    }
+                    var timeStr = j.ToString() + ":00";
+                    if (j == 9)
+                    {
+                        timeStr = "0" + timeStr;
+                    }
+                    calEntry.time = timeStr;
+                    calEntry.stylist = stylist;
+                    calEntry.customer = "";
+                    //  var result = thebook.Find(x => x.date == DateTime.Today.AddDays(i).ToShortDateString() && x.time == timeStr && x.stylist == stylist);
+                    List<string> freestylists = myBookings.GetAvailableStylists(calEntry.date, timeStr);
+                    if (freestylists != null && freestylists.Count > 0)
+                    {
+                        if (j % 2 == 1)
+                            calEntry.customer = freestylists.First();
+                        else
+                            calEntry.customer = freestylists.Last();
+                        //  calEntry.phone = result.phone;
+                        //  calEntry.style = result.style;
+                        //  calEntry.info = result.info;
+                    }
+                    daycal.Add(calEntry);
+                    j++;
+                }
+
+                myDates.Add(DateTime.Today.AddDays(i).ToShortDateString());
+                i++;
+
+            }
+            ViewBag.BookingDates = myDates;
+
+            myDays.Add("Today");
+            i = 1;
+            while (i <= 6)
+            {
+                myDays.Add(DateTime.Today.AddDays(i).DayOfWeek.ToString());
+                i++;
+
+            }
+            ViewBag.BookingDays = myDays;
+            return Json(daycal, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetAppointments(string stylist)
+        {
+
+            List<bookingview> daycal = new List<bookingview>();
+            if (stylist == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.Stylist = stylist;
+
+            Bookings myBookings = new Bookings();
+            List<Booking> thebook = myBookings.GetBookings();
+
+            List<string> myDates = new List<string>();
+            List<string> myDays = new List<string>();
+            //    myDates.Add(DateTime.Today.ToShortDateString());
+            int i = 0;
+            while (i <= 6)
+            {
+                int j = 9;
+                TheListeningHand.Models.TheListeningHandEntities context = new TheListeningHandEntities();
+                List<availability> slots = context.availabilities.Where(s => s.stylistId.ToString() == "1" ).ToList();
+                foreach (availability slot in slots)
+                {
+                    bookingview calEntry = new bookingview();
+                    calEntry.date = DateTime.Today.AddDays(i).ToShortDateString();
+                    if (i == 0)
+                    {
+                        calEntry.datedetail = "Today";
+                    }
+                    else
+                    {
+                        calEntry.datedetail = DateTime.Today.AddDays(i).DayOfWeek.ToString();
+                    }
+                    var timeStr = slot.starttime.ToString() + ":00";
+                    if (j < 10)
+                    {
+                        timeStr = "0" + timeStr;
+                    }
+                    calEntry.time = timeStr;
+                    calEntry.stylist = stylist;
+                    calEntry.customer = "";
+                    var result = thebook.Find(x => x.date == DateTime.Today.AddDays(i).ToShortDateString() && x.time == timeStr && x.stylist == stylist);
+
+                    if (result != null)
+                    {
+                        calEntry.customer = result.name;
+                        calEntry.phone = result.phone;
+                        calEntry.style = result.style;
+                        calEntry.info = result.info;
+                    }
+                    daycal.Add(calEntry);
+                    j++;
+                }
+
+                myDates.Add(DateTime.Today.AddDays(i).ToShortDateString());
+                i++;
+
+            }
+            ViewBag.BookingDates = myDates;
+
+            myDays.Add("Today");
+            i = 1;
+            while (i <= 6)
+            {
+                myDays.Add(DateTime.Today.AddDays(i).DayOfWeek.ToString());
+                i++;
+
+            }
+            ViewBag.BookingDays = myDays;
+            return Json(daycal, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult AddAppointment(string name, string phone, string style, string info, string stylist, string date, string time)
+        {
+            Bookings myBookings = new Bookings();
+            myBookings.AddBooking(name, phone, style, info, stylist, date, time);
+            //  myBookings.GetBookings();
+            return Json(myBookings.currentBookings, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult GetAllAppointments()
+        {
+            Bookings myBookings = new Bookings();
+            var thebook = myBookings.GetBookings();
+            return Json(thebook, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult GoToAppointment(string stylist)
+        {
+            return RedirectToAction("Appointment", new { stylist = stylist });
+        }
+
+        public ActionResult Hairdresser(string stylist)
+        {
+            List<bookingview> daycal = new List<bookingview>();
+            if (stylist == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.Stylist = stylist;
+
+
+
+            return View();
+        }
+        public ActionResult Management()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Splash()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Anyone(string stylist)
+        {
+
+            List<bookingview> daycal = new List<bookingview>();
+            if (stylist == null)
+            {
+                ///   return RedirectToAction("Index");
+                stylist = "Anyone Available";
+            }
+            ViewBag.Stylist = stylist;
+
+            Bookings myBookings = new Bookings();
+            List<Booking> thebook = myBookings.GetBookings();
+
+            List<string> myDates = new List<string>();
+            List<string> myDays = new List<string>();
+            //    myDates.Add(DateTime.Today.ToShortDateString());
+            int i = 0;
+            while (i <= 6)
+            {
+                int j = 9;
+                while (j <= 17)
+                {
+                    bookingview calEntry = new bookingview();
+                    calEntry.date = DateTime.Today.AddDays(i).ToShortDateString();
+                    if (i == 0)
+                    {
+                        calEntry.datedetail = "Today";
+                    }
+                    else
+                    {
+                        calEntry.datedetail = DateTime.Today.AddDays(i).DayOfWeek.ToString();
+                    }
+                    var timeStr = j.ToString() + ":00";
+                    if (j == 9)
+                    {
+                        timeStr = "0" + timeStr;
+                    }
+                    calEntry.time = timeStr;
+                    calEntry.stylist = stylist;
+                    calEntry.customer = "";
+                    var result = thebook.Find(x => x.date == DateTime.Today.AddDays(i).ToShortDateString() && x.time == timeStr); // && x.stylist == stylist);
+
+                    //   string freestylists = myBookings.GetAvailableStylists(calEntry.date, timeStr);
+                    if (result != null)
+                    {
+                        calEntry.customer = result.name;
+                        //  calEntry.phone = result.phone;
+                        //  calEntry.style = result.style;
+                        //  calEntry.info = result.info;
+                    }
+                    daycal.Add(calEntry);
+                    j++;
+                }
+
+                myDates.Add(DateTime.Today.AddDays(i).ToShortDateString());
+                i++;
+
+            }
+            ViewBag.BookingDates = myDates;
+
+            myDays.Add("Today");
+            i = 1;
+            while (i <= 6)
+            {
+                myDays.Add(DateTime.Today.AddDays(i).DayOfWeek.ToString());
+                i++;
+
+            }
+            ViewBag.BookingDays = myDays;
+            return View("Anyone");
+
         }
     }
 }
---
+++ TheListeningHand/Models/Bookings.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheListeningHand.Models
+{
+    public class Booking
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string style { get; set; }
+        public string info { get; set; }
+        public string stylist { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+
+    }
+    public class Bookings
+    {
+        public List<Booking> currentBookings { get; set; }
+        public Bookings()
+        {
+            HttpContext context = HttpContext.Current;
+            if (context.Session["bookings"] != null)
+            {
+                currentBookings = (List<Booking>)context.Session["bookings"];
+            }
+            else
+            {
+                currentBookings = new List<Booking>();
+
+                Booking addBooking = new Booking();
+                addBooking.name = "Jackie";
+                addBooking.phone = "0766554433";
+                addBooking.style = "Perm";
+                addBooking.info = "Frizzy hair";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.ToShortDateString();
+                addBooking.time = "09:00";
+                currentBookings.Add(addBooking);
+                addBooking = new Booking();
+                addBooking.name = "Sam";
+                addBooking.phone = "0700766433";
+                addBooking.style = "Wet Cut";
+                addBooking.info = "Very thin hair";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.ToShortDateString();
+                addBooking.time = "10:00";
+                currentBookings.Add(addBooking);
+                addBooking = new Booking();
+                addBooking.name = "Amanda";
+                addBooking.phone = "07225544353";
+                addBooking.style = "Colour treatment";
+                addBooking.info = "Second phase";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.ToShortDateString();
+                addBooking.time = "12:00";
+                currentBookings.Add(addBooking);
+                addBooking = new Booking();
+                addBooking.name = "Jackie's Mum";
+                addBooking.phone = "0766554433";
+                addBooking.style = "Perm";
+                addBooking.info = "Frizzy hair";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.ToShortDateString();
+                addBooking.time = "14:00";
+                currentBookings.Add(addBooking);
+                addBooking = new Booking();
+                addBooking.name = "Gita";
+                addBooking.phone = "0766554433";
+                addBooking.style = "Perm";
+                addBooking.info = "Frizzy hair";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.ToShortDateString();
+                addBooking.time = "15:00";
+                currentBookings.Add(addBooking);
+                addBooking = new Booking();
+                addBooking.name = "Gita's Mum";
+                addBooking.phone = "0766554433";
+                addBooking.style = "Perm";
+                addBooking.info = "Frizzy hair";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.AddDays(1).ToShortDateString();
+                addBooking.time = "09:00";
+                currentBookings.Add(addBooking);
+                addBooking = new Booking();
+                addBooking.name = "Mrs Mop";
+                addBooking.phone = "0766554433";
+                addBooking.style = "Perm";
+                addBooking.info = "Frizzy hair";
+                addBooking.stylist = "Tina Sparkle";
+                addBooking.date = DateTime.Today.AddDays(1).ToShortDateString();
+                addBooking.time = "10:00";
+                currentBookings.Add(addBooking);
+
+                context.Session["bookings"] = currentBookings;
+            }
+        }
+        public List<Booking> AddBooking(string name, string phone, string style, string info, string stylist, string date, string time)
+        {
+            HttpContext context = HttpContext.Current;
+            Booking addBooking = new Booking();
+            addBooking.name = name;
+            addBooking.phone = phone;
+            addBooking.style = style;
+            addBooking.info = info;
+            addBooking.stylist = stylist;
+            addBooking.date = date;
+            addBooking.time = time;
+            currentBookings.Add(addBooking);
+            context.Session["bookings"] = currentBookings;
+            return currentBookings;
+        }
+        public List<Booking> GetBookings()
+        {
+            HttpContext context = HttpContext.Current;
+            currentBookings = (List<Booking>)context.Session["bookings"];
+            currentBookings = currentBookings.OrderBy(o => o.date).ThenBy(c => c.time).ThenBy(c => c.stylist).ToList();
+            return currentBookings;
+        }
+
+        public List<string> GetAvailableStylists(string date, string time)
+        {
+
+            HttpContext context = HttpContext.Current;
+            currentBookings = (List<Booking>)context.Session["bookings"];
+            List<string> myStrings = new List<string>();
+            string output = "";
+            var result = currentBookings.Find(x => x.date == date && x.time == time && x.stylist == "Tina Sparkle");
+            if (result == null)
+            {
+                output = output + " " + "Tina Sparkle";
+                myStrings.Add("Tina Sparkle");
+            }
+            result = currentBookings.Find(x => x.date == date && x.time == time && x.stylist == "Frank Ferret");
+            if (result == null)
+            {
+                output = output + " " + "Frank Ferret";
+                myStrings.Add("Frank Ferret");
+            }
+            result = currentBookings.Find(x => x.date == date && x.time == time && x.stylist == "Marie");
+            if (result == null)
+            {
+                output = output + " " + "Marie";
+                myStrings.Add("Marie");
+            }
+            result = currentBookings.Find(x => x.date == date && x.time == time && x.stylist == "Holly Bush");
+
+            if (result == null)
+            {
+                output = output + " " + "Holly Bush";
+                myStrings.Add("Holly Bush");
+            }
+
+            return myStrings;
+        }
+    }
+}
